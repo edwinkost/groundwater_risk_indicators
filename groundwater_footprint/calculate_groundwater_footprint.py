88f86f3ae20be2029ef1a36fad52092bdddde3d2
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-03-21 12:21:42 +0100

    first check

--- groundwater_footprint/calculate_groundwater_footprint.py
+++ groundwater_footprint/calculate_groundwater_footprint.py
@@ -87,7 +87,7 @@ fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, 0.1)
 # - set minimum value to 0.10
 fraction_reserved_recharge = pcr.max(0.10, fraction_reserved_recharge)
 # - set maximum value to 0.75
-fraction_reserved_recharge = pcr.min(0.75, fraction_reserved_recharge)
+fraction_reserved_recharge = pcr.min(1.00, fraction_reserved_recharge)
 
 # areal_groundwater_abstraction (unit: m/year)
 #~ groundwater_abstraction = pcr.cover(pcr.readmap("/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/rapid/edwin/05min_runs_results/2015_04_27/non_natural_2015_04_27/global/analysis/avg_values_1990_to_2010/totalGroundwaterAbstraction_annuaTot_output_1990to2010.map"), 0.0)
@@ -125,7 +125,7 @@ groundwater_contribution_to_environmental_flow          = pcr.max(0.10, fraction
 groundwater_contribution_to_environmental_flow_filename = output_directory + "/" + "groundwater_contribution_to_environmental_flow.m.per.year.map" 
 pcr.report(pcr.ifthen(landmask, groundwater_contribution_to_environmental_flow), groundwater_contribution_to_environmental_flow_filename)
 areal_groundwater_contribution_to_environmental_flow = pcr.areatotal(groundwater_contribution_to_environmental_flow * cell_area, class_map)/pcr.areatotal(cell_area, class_map) 
-areal_groundwater_contribution_to_environmental_flow = pcr.min(0.75 * areal_groundwater_recharge, areal_groundwater_contribution_to_environmental_flow)
+areal_groundwater_contribution_to_environmental_flow = pcr.min(1.00 * areal_groundwater_recharge, areal_groundwater_contribution_to_environmental_flow)
 
 # groundwater stress map (dimensionless)
 groundwater_stress_map = pcr.ifthen(landmask, \
