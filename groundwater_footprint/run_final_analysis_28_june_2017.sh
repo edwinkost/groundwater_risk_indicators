3101689eeeffa1149d6aaffe7d8259e412fc42ad
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-06-28 16:04:27 +0200

    Finalizing.

---
+++ groundwater_footprint/run_final_analysis_28_june_2017.sh
@@ -0,0 +1,44 @@
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2010/ pixel 1990 2010 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2014/ pixel 1990 2014 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2015/ pixel 1990 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2000-2015/ pixel 2000 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1960-1990/ pixel 1960 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1970-1990/ pixel 1970 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2011-2015/ pixel 2011 2015 &
+wait
+
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2010/ country 1990 2010 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2014/ country 1990 2014 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2015/ country 1990 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2000-2015/ country 2000 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1960-1990/ country 1960 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1970-1990/ country 1970 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2011-2015/ country 2011 2015 &
+wait
+
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2010/ state 1990 2010 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2014/ state 1990 2014 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2015/ state 1990 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2000-2015/ state 2000 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1960-1990/ state 1960 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1970-1990/ state 1970 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2011-2015/ state 2011 2015 &
+wait
+
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2010/ drainage_unit 1990 2010 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2014/ drainage_unit 1990 2014 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2015/ drainage_unit 1990 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2000-2015/ drainage_unit 2000 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1960-1990/ drainage_unit 1960 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1970-1990/ drainage_unit 1970 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2011-2015/ drainage_unit 2011 2015 &
+wait
+
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2010/ aquifer 1990 2010 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2014/ aquifer 1990 2014 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1990-2015/ aquifer 1990 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2000-2015/ aquifer 2000 2015 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1960-1990/ aquifer 1960 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/1970-1990/ aquifer 1970 1990 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinsut/groundwater_footprint_final_28_june_2017/2011-2015/ aquifer 2011 2015 &
+wait
