99f0ed43df828fe3959ad6934bab7c23f305968c
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-06-28 15:17:40 +0200

    Finalizing.

--- groundwater_footprint/calculate_groundwater_footprint.py
+++ groundwater_footprint/calculate_groundwater_footprint.py
@@ -82,8 +82,8 @@ fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, \
 fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, \
                                        pcr.windowaverage(fraction_reserved_recharge, 0.5))
 fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, 0.1)
-# - set minimum value to 0.25
-fraction_reserved_recharge = pcr.max(0.25, fraction_reserved_recharge)
+# - set minimum value to 0.10
+fraction_reserved_recharge = pcr.max(0.10, fraction_reserved_recharge)
 # - set maximum value to 0.75
 fraction_reserved_recharge = pcr.min(0.75, fraction_reserved_recharge)
 
@@ -133,8 +133,8 @@ areal_groundwater_recharge = pcr.areatotal(groundwater_recharge * cell_area, cla
 areal_groundwater_recharge = pcr.max(0.0, areal_groundwater_recharge)
 
 # areal groundwater contribution to meet enviromental flow (unit: m/year)
-#~ groundwater_contribution_to_environmental_flow          = pcr.max(0.10, fraction_reserved_recharge * groundwater_recharge)
-groundwater_contribution_to_environmental_flow          = fraction_reserved_recharge * groundwater_recharge
+groundwater_contribution_to_environmental_flow          = pcr.max(0.10, fraction_reserved_recharge * groundwater_recharge)
+#~ groundwater_contribution_to_environmental_flow       = fraction_reserved_recharge * groundwater_recharge
 groundwater_contribution_to_environmental_flow_filename = output_directory + "/" + "groundwater_contribution_to_environmental_flow.m.per.year.map" 
 pcr.report(pcr.ifthen(landmask, groundwater_contribution_to_environmental_flow), groundwater_contribution_to_environmental_flow_filename)
 areal_groundwater_contribution_to_environmental_flow = pcr.areatotal(groundwater_contribution_to_environmental_flow * cell_area, class_map)/pcr.areatotal(cell_area, class_map) 
