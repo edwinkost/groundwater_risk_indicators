aa5ca08db46cabd23605ca3ff2f3ff7a1317f6d8
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-07-14 12:15:57 +0200

    Finalizing groundwater stress maps.

--- groundwater_footprint/bash_calculate_groundwater_footprint.sh
+++ groundwater_footprint/bash_calculate_groundwater_footprint.sh
@@ -1,7 +1,7 @@
 
-python calculate_groundwater_footprint.py /scratch-shared/edwinhs/groundwater_foot_print_july_2017/groundwater_stress_1990-2014/state/         state         1990 1991 &
-python calculate_groundwater_footprint.py /scratch-shared/edwinhs/groundwater_foot_print_july_2017/groundwater_stress_1990-2014/pixel/         pixel         1990 1991 &
-python calculate_groundwater_footprint.py /scratch-shared/edwinhs/groundwater_foot_print_july_2017/groundwater_stress_1990-2014/aquifer/       aquifer       1990 1991 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinhs/groundwater_foot_print_july_2017/groundwater_stress_1990-2014/state/ state 1990 1991 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinhs/groundwater_foot_print_july_2017/groundwater_stress_1990-2014/pixel/ pixel 1990 1991 &
+python calculate_groundwater_footprint.py /scratch-shared/edwinhs/groundwater_foot_print_july_2017/groundwater_stress_1990-2014/aquifer/ aquifer 1990 1991 &
 python calculate_groundwater_footprint.py /scratch-shared/edwinhs/groundwater_foot_print_july_2017/groundwater_stress_1990-2014/drainage_unit/ drainage_unit 1990 1991 &
 wait
 
